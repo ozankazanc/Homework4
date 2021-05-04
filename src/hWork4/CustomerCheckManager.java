31c0c98aef1144592c8edf340ff788d3e1dd4f09
Ozan Kazanç <ozan.kazanc@hotmail.com>
2021-05-05 01:18:44 +0300

    Added Abstract Folder

--- src/hWork4/CustomerCheckManager.java
+++ src/hWork4/CustomerCheckManager.java
@@ -1,9 +1,12 @@
 package hWork4;
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
 
 public class CustomerCheckManager implements ICustomerCheckService {
 
 	@Override
 	public boolean CheckIfRealPerson(Customer customer) {
+		
+		//KPSPublicSoapProxy soap
 		
 		return true;
 	}
